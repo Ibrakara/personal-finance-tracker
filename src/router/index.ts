e48531c1b9c55d186e0bac78e91b0b1c32b3d52c
Ibrahim Karaduman <s4c.karaduman@mercedes-benz.com>
2025-02-15 22:52:28 +0300

    delete template resources

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView,
+      path: '/list',
+      name: 'list',
+      component: () => import('@/views/ListView.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/',
+      name: 'analytics',
+      component: () => import('@/views/AnalyticsView.vue'),
+    },
+    {
+      path: '/create',
+      name: 'transaction entry',
+      component: () => import('@/views/TransactionFormView.vue'),
     },
   ],
 })
