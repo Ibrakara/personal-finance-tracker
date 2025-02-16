7cd9e4789b94f3ff44fda3fbedfbe88c51282aec
Ibrahim Karaduman <s4c.karaduman@mercedes-benz.com>
2025-02-16 16:25:13 +0300

    fix styling

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -1,8 +1,7 @@
 <template>
   <button
     type="button"
-    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-    :class="{ 'opacity-40': disabled, 'cursor-pointer': !disabled }"
+    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 disabled:opacity-40 cursor-pointer disabled:cursor-default"
     :disabled="disabled"
     @click="onClick"
   >
