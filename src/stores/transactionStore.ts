4453cd1e2aadd0e1c2ab7085d759616b7e8f434d
Ibrahim Karaduman <s4c.karaduman@mercedes-benz.com>
2025-02-16 09:04:08 +0300

    add form date and transaction list

--- src/stores/transactionStore.ts
+++ src/stores/transactionStore.ts
@@ -1,12 +1,20 @@
-import { computed, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { defineStore } from 'pinia'
-import type { EXPENSE_CATAGORIES, INCOME_CATAGORIES, TRANSACTION_TYPE } from '@/types'
+import {
+  type Expense,
+  type EXPENSE_CATAGORIES,
+  type Income,
+  type INCOME_CATAGORIES,
+  type TRANSACTION_TYPE,
+} from '@/types'
 
 export const useTransactionStore = defineStore('transactionStore', () => {
   //states
   const transactionFormCategory = ref<EXPENSE_CATAGORIES | INCOME_CATAGORIES>()
   const transactionFormType = ref<TRANSACTION_TYPE>()
   const transactionFormAmount = ref<number>()
+  const transactionFormDate = ref<string>()
+  const transactionList = ref<Array<Expense | Income>>([])
   //computeds
   const expenseCategoryList = computed(() => {
     return [
@@ -30,19 +38,55 @@ export const useTransactionStore = defineStore('transactionStore', () => {
   const transactionTypeList = computed(() => {
     return ['EXPENSE', 'INCOME']
   })
+  const getTransactionList = computed(() => {
+    return transactionList.value
+  })
+
+  //methods
   function createTransaction() {
-    console.log('clicked')
+    const transactionObject: Expense | Income = {
+      date: transactionFormDate.value!,
+      amout: transactionFormAmount.value!,
+      type: transactionFormType.value!,
+      category: transactionFormCategory.value!,
+    }
+    transactionList.value.push(transactionObject)
+    setLocalStorage()
+    $resetTransactionForm()
+    console.log(transactionList.value)
   }
+  function setLocalStorage() {
+    const stringifiedTransactionList = JSON.stringify(transactionList.value)
+    localStorage.setItem('transactionList', stringifiedTransactionList)
+  }
+  function getFromLocalStorage() {
+    const stringifiedTransactionList = localStorage.getItem('transactionList')
+    if (stringifiedTransactionList) {
+      const transactionListArray = JSON.parse(stringifiedTransactionList) as Array<Expense | Income>
+      transactionList.value = transactionListArray
+    }
+  }
+  function $resetTransactionForm() {
+    transactionFormCategory.value = undefined
+    transactionFormType.value = undefined
+    transactionFormAmount.value = undefined
+    transactionFormDate.value = undefined
+  }
+  onMounted(() => {
+    getFromLocalStorage()
+  })
 
   return {
     //states
     transactionFormCategory,
     transactionFormType,
     transactionFormAmount,
+    transactionFormDate,
     //computeds
     expenseCategoryList,
     incomeCategoryList,
     transactionTypeList,
+    getTransactionList,
     //methods
     createTransaction,
   }
--- src/views/TransactionFormView.vue
+++ src/views/TransactionFormView.vue
@@ -3,7 +3,7 @@
     <div class="w-full">
       <h5 class="text-center text-3xl font-semibold text-slate-500">Create Transaction</h5>
     </div>
-    <form class="min-w-xs w-full mx-auto min-h-72 flex flex-col justify-between items-center">
+    <form class="min-w-xs w-full mx-auto h-auto gap-3 flex flex-col justify-between items-center">
       <CustomDropdown
         v-model="transactionFormType"
         class="min-w-80 max-sm:w-3xs w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
@@ -22,8 +22,15 @@
         type="number"
         label="Amount"
       ></CustomInput>
+      <CustomInput
+        v-model="transactionFormDate"
+        class="min-w-80 max-sm:w-3xs w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg"
+        type="date"
+        label="Date"
+      ></CustomInput>
+      {{ transactionFormDate }}
       <CustomButton
-        :disabled="!transactionFormCategory || !transactionFormType || !transactionFormAmount"
+        :disabled="isCreateButtonDisabled"
         name="Create transaction"
         @on-click="createTransaction"
       ></CustomButton>
@@ -42,6 +49,7 @@ const {
   transactionFormCategory,
   transactionFormType,
   transactionFormAmount,
+  transactionFormDate,
   expenseCategoryList,
   incomeCategoryList,
   transactionTypeList,
@@ -51,6 +59,14 @@ const transactionCategoryList = computed(() => {
   return transactionFormType.value !== 'INCOME'
     ? expenseCategoryList.value
     : incomeCategoryList.value
+})
+const isCreateButtonDisabled = computed(() => {
+  return (
+    !transactionFormCategory.value ||
+    !transactionFormType.value ||
+    !transactionFormAmount.value ||
+    !transactionFormDate.value
+  )
 })
 watch(
   () => transactionFormType.value,
