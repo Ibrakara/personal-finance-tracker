48edaa40c57d23baf1a82b7b15e8b10dc69884be
Ibrahim Karaduman <s4c.karaduman@mercedes-benz.com>
2025-02-16 17:45:39 +0300

    create card component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="w-xl bg-white border border-gray-200 rounded-xl shadow-md overflow-hidden">
+    <div class="p-4 flex justify-center items-center flex-col">
+      <h3 class="text-lg font-semibold text-gray-900">{{ title }}</h3>
+      <p class="text-gray-600 mt-2">{{ description }}</p>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+interface CardProps {
+  title: string
+  description?: string
+  imageUrl?: string
+}
+defineProps<CardProps>()
+</script>
