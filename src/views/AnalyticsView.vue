fd628c08fcf317ab2aab416eb7adff34ab81d789
Ibrahim Karaduman <s4c.karaduman@mercedes-benz.com>
2025-02-17 14:24:54 +0300

    add gaps

--- src/views/AnalyticsView.vue
+++ src/views/AnalyticsView.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="flex flex-col w-[80vw] justify-start gap-6 items-center h-[80vh]">
     <h1 class="text-center text-3xl font-semibold text-slate-500">Analytics</h1>
-    <div
-      class="flex flex-col w-full justify-around items-center bg-gray-50 border border-gray-100 rounded-l shadow-md overflow-hidden h-96"
-    >
+    <div class="flex flex-col w-full justify-around items-center gap-3 overflow-hidden min-h-96">
       <div class="flex justify-around w-full gap-2">
         <CardComponent title="Income" :description="`${totalIncome ? totalIncome : '-'} TL`" />
         <CardComponent title="Expense" :description="`${totalExpense ? totalExpense : '-'} TL`" />
         <CardComponent title="Balance" :description="`${balance ? balance : '-'} TL`" />
       </div>
-      <div class="flex justify-around w-full">
+      <div class="flex justify-around w-full gap-2">
         <CardComponent title="Income by Category">
           <div class="h-48 w-full">
             <Doughnut :data="categoryChartDataByType(TRANSACTION_TYPE.INCOME)" :options="options" />
