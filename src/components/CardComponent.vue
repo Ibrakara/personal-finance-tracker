a8448b8c00efc3c8f0be9dfaad344db7de604abe
Ibrahim Karaduman <s4c.karaduman@mercedes-benz.com>
2025-02-17 16:47:53 +0300

    add condition for tag

--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -2,7 +2,7 @@
   <div class="w-xl bg-white border border-gray-200 rounded-xl shadow-md overflow-hidden">
     <div class="p-4 flex justify-center items-center flex-col">
       <h3 class="text-lg font-semibold text-gray-900">{{ title }}</h3>
-      <p class="text-gray-600 mt-2">{{ description }}</p>
+      <p v-if="description" class="text-gray-600 mt-2">{{ description }}</p>
       <slot></slot>
     </div>
   </div>
