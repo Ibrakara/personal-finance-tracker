d983530f66fb9a880db4ac35a4665e2579eac2e3
Ibrahim Karaduman <s4c.karaduman@mercedes-benz.com>
2025-02-17 11:10:39 +0300

    create chart related helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,5 @@
+import type { EXPENSE_CATAGORIES, INCOME_CATAGORIES, Transaction, TRANSACTION_TYPE } from '@/types'
+
 export const capitalizeFirstLetter = (value: string | unknown) => {
   if (typeof value === 'string') {
     return value ? value[0].toUpperCase() + value.substring(1)?.toLowerCase() : ''
@@ -14,4 +16,33 @@ export const formatDate = (date: Date | string): string => {
   const year = dateString.getFullYear()
 
   return `${day}-${month}-${year}`
+}
+export function populateCategoryMapByTransactionType(
+  transactionType: TRANSACTION_TYPE,
+  transactionList: Transaction[],
+) {
+  const categoryMap: Map<INCOME_CATAGORIES | EXPENSE_CATAGORIES, number> = new Map()
+  transactionList.forEach((transaction) => {
+    if (transaction.type === transactionType) {
+      const category = transaction.category
+      if (categoryMap.has(category)) {
+        const categoryPreviousAmount = categoryMap.get(category)
+        const categoryTotalAmount = categoryPreviousAmount! + transaction.amount
+        categoryMap.set(category, categoryTotalAmount)
+      } else {
+        categoryMap.set(category, transaction.amount)
+      }
+    }
+  })
+  return categoryMap
+}
+export function getRandomHexColorListByCount(count: number): string[] {
+  const MAX_HEX_COLOR = 16_777_215
+  return Array.from(
+    { length: count },
+    () =>
+      `#${Math.floor(Math.random() * MAX_HEX_COLOR)
+        .toString(16)
+        .padStart(6, '0')}`,
+  )
 }
